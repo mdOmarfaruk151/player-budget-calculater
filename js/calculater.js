c0f192034b577af83c82fd072a61391a3e02953f
mdOmarfaruk151 <mdomar28151@gmile.com>
2022-08-31 04:18:05 +0600

    added manager and coach js function

--- js/calculater.js
+++ js/calculater.js
@@ -8,12 +8,34 @@ document.getElementById('btn-per-total').addEventListener('click', function(){
   
     const outputTotalElement = document.getElementById('player-expenses');
     const previousOutputTotalString = outputTotalElement.innerText;
-    const previousOutputTotal = parseFloat(previousOutputTotalString);
+    // const previousOutputTotal = parseFloat(previousOutputTotalString);
   
     const outputAddedElement = document.getElementById('total-added-player');
-    const previousOutputAddedString = outputAddedElement.innerText;
-    const previousAddedTotal = parseFloat(previousOutputAddedString);
+    const playerOutputAddedString = outputAddedElement.innerText;
+    const playerAddedTotal = parseFloat(playerOutputAddedString);
   
-    const newOutputTotal = previousAddedTotal * newPlayerAmount;
-    outputTotalElement.innerText = newOutputTotal; 
+    const playerExpensesTotal = playerAddedTotal * newPlayerAmount;
+    outputTotalElement.innerText = playerExpensesTotal; 
+
+    
+  })
+
+  document.getElementById('btn-calculate-total').addEventListener('click', function(){
+    const managerInputField = document.getElementById('manager-input-field');
+    const managerAmountString = managerInputField.value;
+    const managerInputAmount = parseFloat(managerAmountString);
+    // console.log(managerInputAmount);
+  
+    managerInputField.value = '';
+
+  })
+
+  document.getElementById('btn-calculate-total').addEventListener('click', function(){
+    const coachInputField = document.getElementById('coach-input-field');
+    const coachAmountString = coachInputField.value;
+    const coachInputAmount = parseFloat(coachAmountString);
+    // console.log(coachInputAmount);
+  
+    coachInputField.value = '';
+
   })
