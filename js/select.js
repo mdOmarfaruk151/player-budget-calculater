a1b45db4062bf0081135a26b96c4e7fd7e36631a
mdOmarfaruk151 <mdomar28151@gmile.com>
2022-08-30 21:11:40 +0600

    player select js function added

---
+++ js/select.js
@@ -0,0 +1,36 @@
+const playersArray = [];
+
+function display(player) {
+  console.log(player);
+  const tableBody = document.getElementById("selected-players");
+  tableBody.innerHTML = "";
+  for (let i = 0; i < player.length; i++) {
+    const name = player[i].playerName;
+
+    const tr = document.createElement("tr");
+
+    //  use ${item name} to show that one
+    tr.innerHTML = `
+         <th>${i + 1}</th>
+         <td>${name}</td>
+                                
+         `;
+
+    tableBody.appendChild(tr);
+  }
+}
+
+function addToPlayer(element) {
+  const playerName = element.parentNode.parentNode.children[0].innerText;
+
+  const playerObject = {
+    playerName: playerName,
+  };
+
+  playersArray.push(playerObject);
+  // console.log(playersArray);
+  // console.log(playersArray.length);
+  document.getElementById("total-added-player").innerText = playersArray.length;
+
+  display(playersArray);
+}
